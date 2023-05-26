5f8f4c37e5bf93d94850a8be6cf2508fd711ef53
DiegoBelPe <dbeltranpereira@gmail.com>
2023-05-25 23:05:51 -0500

    add select pets logic

--- js/poke.js
+++ js/poke.js
@@ -1,6 +1,17 @@
-function selectPets(){
-  alert('hello button select pets')
+function selectPets() {
+  const radioOne = document.getElementById("hipodoge");
+  const radioTwo = document.getElementById("capipepo");
+  const radioThree = document.getElementById("ratigueya");
+  if (radioOne.checked) {
+    alert("hipodoge");
+  } else if (radioTwo.checked) {
+    alert("capipepo");
+  } else if (radioThree.checked) {
+    alert("ratigueya");
+  } else {
+    alert(":(");
+  }
 }
-let buttonPet = document.getElementById('button-pet')
+let buttonPet = document.getElementById("button-pet");
 
-buttonPet.addEventListener('click', selectPets)
+buttonPet.addEventListener("click", selectPets);
