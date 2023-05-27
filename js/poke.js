a24cb16693c546f2d8454357589fef7ccc58e970
DiegoBelPe <dbeltranpereira@gmail.com>
2023-05-26 20:38:56 -0500

    add functions for select pets

--- js/poke.js
+++ js/poke.js
@@ -1,17 +1,44 @@
+let buttonPet = document.getElementById("button-pet");
+
+buttonPet.addEventListener("click", selectPets);
+
 function selectPets() {
   const radioOne = document.getElementById("hipodoge");
   const radioTwo = document.getElementById("capipepo");
   const radioThree = document.getElementById("ratigueya");
+  const spanPet = document.getElementById("name-pet");
   if (radioOne.checked) {
-    alert("hipodoge");
+    spanPet.innerHTML = 'Hipodoge'
   } else if (radioTwo.checked) {
-    alert("capipepo");
+    spanPet.innerHTML = 'Capipepo'
   } else if (radioThree.checked) {
-    alert("ratigueya");
+    spanPet.innerHTML = 'Ratigueya'
   } else {
-    alert(":(");
+    alert("Please select pet");
   }
-}
-let buttonPet = document.getElementById("button-pet");
 
-buttonPet.addEventListener("click", selectPets);
+  selectPetEnemy()
+}
+
+function selectPetEnemy(){
+  let attackPetRandom = randomEnemy(1, 3);
+  let nameEnemy = document.getElementById('name-petEnemy')
+
+  if(attackPetRandom == 1){
+    nameEnemy.innerHTML = 'Hipodoge'
+  }
+  if(attackPetRandom == 2){
+    nameEnemy.innerHTML = 'Capipepo'
+  }
+  if(attackPetRandom == 3){
+    nameEnemy.innerHTML = 'Ratiguaye'
+  }
+
+}
+
+
+function randomEnemy(min, max) {
+  return Math.floor(Math.random() * (max - min +1) + min)
+  
+}
+
