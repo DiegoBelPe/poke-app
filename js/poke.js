b18154248babec67c97dba092b8e9ffdcd79e25f
DiegoBelPe <dbeltranpereira@gmail.com>
2023-06-07 22:00:34 -0500

    add player life counter

--- js/poke.js
+++ js/poke.js
@@ -1,19 +1,18 @@
-
 let playerAttack;
 let enemyAttack;
+let livesPetPlayer = 3;
+let livesPetEnemy = 3;
 
-
-function startGame (){
+function startGame() {
   const buttonPet = document.getElementById("button-pet");
   buttonPet.addEventListener("click", selectPets);
 
-  const buttonFire = document.getElementById("power-fire")
-  buttonFire.addEventListener('click', fireAttack);
-  const buttonWater = document.getElementById("power-water")
-  buttonWater.addEventListener('click', waterAttack) 
-  const buttonLand = document.getElementById("power-land")
-  buttonLand.addEventListener('click', landAttack) 
-  
+  const buttonFire = document.getElementById("power-fire");
+  buttonFire.addEventListener("click", fireAttack);
+  const buttonWater = document.getElementById("power-water");
+  buttonWater.addEventListener("click", waterAttack);
+  const buttonLand = document.getElementById("power-land");
+  buttonLand.addEventListener("click", landAttack);
 }
 function selectPets() {
   const radioOne = document.getElementById("hipodoge");
@@ -21,90 +20,96 @@ function selectPets() {
   const radioThree = document.getElementById("ratigueya");
   const spanPet = document.getElementById("name-pet");
   if (radioOne.checked) {
-    spanPet.innerHTML = 'Hipodoge'
+    spanPet.innerHTML = "Hipodoge";
   } else if (radioTwo.checked) {
-    spanPet.innerHTML = 'Capipepo'
+    spanPet.innerHTML = "Capipepo";
   } else if (radioThree.checked) {
-    spanPet.innerHTML = 'Ratigueya'
+    spanPet.innerHTML = "Ratigueya";
   } else {
     alert("Please select pet");
   }
 
-  selectPetEnemy()
+  selectPetEnemy();
 }
 
-function selectPetEnemy(){
+function selectPetEnemy() {
   let attackPetRandom = randomEnemy(1, 3);
-  let nameEnemy = document.getElementById('name-petEnemy')
+  let nameEnemy = document.getElementById("name-petEnemy");
 
-  if(attackPetRandom == 1){
-    nameEnemy.innerHTML = 'Hipodoge'
+  if (attackPetRandom == 1) {
+    nameEnemy.innerHTML = "Hipodoge";
   }
-  if(attackPetRandom == 2){
-    nameEnemy.innerHTML = 'Capipepo'
+  if (attackPetRandom == 2) {
+    nameEnemy.innerHTML = "Capipepo";
   }
-  if(attackPetRandom == 3){
-    nameEnemy.innerHTML = 'Ratiguaye'
+  if (attackPetRandom == 3) {
+    nameEnemy.innerHTML = "Ratiguaye";
   }
-
 }
-
 
 function randomEnemy(min, max) {
-  return Math.floor(Math.random() * (max - min +1) + min)
-  
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function fireAttack(){
-  playerAttack = 'Fire'
-  enemyAttackRamdon()
+function fireAttack() {
+  playerAttack = "Fire";
+  enemyAttackRamdon();
 }
-function waterAttack(){
-  playerAttack = 'Water'
-  enemyAttackRamdon()
+function waterAttack() {
+  playerAttack = "Water";
+  enemyAttackRamdon();
 }
-function landAttack(){
-  playerAttack = 'Land'
-  enemyAttackRamdon()
+function landAttack() {
+  playerAttack = "Land";
+  enemyAttackRamdon();
 }
 
-function enemyAttackRamdon(){
+function enemyAttackRamdon() {
   let enemyAttackRand = randomEnemy(1, 3);
 
-  if(enemyAttackRand === 1){
-    enemyAttack = 'Fire'
+  if (enemyAttackRand === 1) {
+    enemyAttack = "Fire";
   }
-  if(enemyAttackRand == 2){
-    enemyAttack = 'Water'
-  }else{
-    enemyAttack = 'Land'
+  if (enemyAttackRand == 2) {
+    enemyAttack = "Water";
+  } else {
+    enemyAttack = "Land";
   }
-  combat()
+  combat();
 }
 
-function createMessage(resultCombat){
-  let sectionMessages = document.getElementById('messages')
+function createMessage(resultCombat) {
+  let sectionMessages = document.getElementById("messages");
 
-  let paragraph = document.createElement('p')
-  paragraph.innerHTML = `Your pet attacked with ${playerAttack}, your enemy's pet attacked with ${enemyAttack}, ${resultCombat}.`
+  let paragraph = document.createElement("p");
+  paragraph.innerHTML = `Your pet attacked with ${playerAttack}, your enemy's pet attacked with ${enemyAttack}, ${resultCombat}.`;
 
-  sectionMessages.appendChild(paragraph)
-
-
+  sectionMessages.appendChild(paragraph);
 }
 
-function combat(){
-  if(enemyAttack == playerAttack){
-    createMessage('Tie')
-  }else if(playerAttack == 'Fire' &&  enemyAttack == 'Land'){
-       createMessage('You win')
-  }else if(playerAttack == 'Water' &&  enemyAttack == 'Fire'){
-    createMessage('You win')
-  }else if(playerAttack == 'Land' &&  enemyAttack == 'Water'){
-    createMessage('You win')
-  }else{
-    createMessage('you lose')
+function combat() {
+  const livesPlayer = document.getElementById("livesPlayer");
+  const livesEnemy = document.getElementById("livesEnemy");
+
+  if (enemyAttack == playerAttack) {
+    createMessage("Tie");
+  } else if (playerAttack == "Fire" && enemyAttack == "Land") {
+    createMessage("You win");
+    livesPetEnemy--;
+    livesEnemy.innerHTML = livesPetEnemy;
+  } else if (playerAttack == "Water" && enemyAttack == "Fire") {
+    createMessage("You win");
+    livesPetEnemy--;
+    livesEnemy.innerHTML = livesPetEnemy;
+  } else if (playerAttack == "Land" && enemyAttack == "Water") {
+    createMessage("You win");
+    livesPetEnemy--;
+    livesEnemy.innerHTML = livesPetEnemy;
+  } else {
+    createMessage("you lose");
+    livesPetPlayer--;
+    livesPlayer.innerHTML = livesPetPlayer;
   }
 }
 
-window.addEventListener('load', startGame)
+window.addEventListener("load", startGame);
